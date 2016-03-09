f03c00640bfbeeba85a88681151fa2b924fefce6
Geun Hee Park <penspanic@gmail.com>
2016-03-09 15:54:27 +0900

    CameraMove

---
+++ Assets/Scripts/Stage Select/SS_CameraMove.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SS_CameraMove : MonoBehaviour
+{
+    void Awake()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    IEnumerator FloorCh ange(int floor)
+    {
+
+    }
+}
